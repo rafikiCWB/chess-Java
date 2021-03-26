f6ee7d409d9777dd2150422e7cc3a7d9bd86ac77
Rafael Grando <sta_rafinha@hotmail.com>
2021-03-26 12:23:22 -0300

    First class: Possition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,30 @@
 package application;
 
+import boardgame.Position;
+
+import javax.swing.*;
+
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        var pos = new Position(3, 5);
+        System.out.println(pos);
+
+
+
+/* String nome;
+ nome = JOptionPane.showInputDialog(null, "Por favor, digite seu nome", "Atenção",
+         JOptionPane.WARNING_MESSAGE);
+ JOptionPane.showMessageDialog(null, "Nome " + nome);
+ System.exit(0);*/
+/* int valor1, valor2;
+ valor1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor digite o primeiro numero",
+         "soma entre os numeros", JOptionPane.WARNING_MESSAGE));
+ valor2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor digite o segundo numero",
+         "soma entre os numeros", JOptionPane.WARNING_MESSAGE));
+ int total = valor1 + valor2;
+ JOptionPane.showMessageDialog(null, "Total" + total);*/
+
     }
 }
